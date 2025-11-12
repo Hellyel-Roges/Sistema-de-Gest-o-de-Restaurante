f624ae7a4a55115004343d019ba47066a656ca46
Hellyel Roges <hellyelroges@gmail.com>
2025-11-12 07:34:26 -0300

    aliemntando e estilizando o frontend

--- app/routes/paginas_routes.py
+++ app/routes/paginas_routes.py
@@ -1,18 +1,20 @@
 # app/routes/paginas_routes.py
 from flask import Blueprint, render_template
 from app.models.produto import Produto
+from datetime import date
 
+year = date.today().year
 paginas_bp = Blueprint("paginas", __name__)
 
 @paginas_bp.route("/")
 def home():
-    return render_template("index.html", title="La Forchetta 🍝")
+    return render_template("index.html", title="La Forchetta 🍝", year=year)
 
 @paginas_bp.route("/reserva_mesa")
 def reserva_mesa_page():
-    return render_template("reserva_mesa.html", title="Reservas - La Forchetta")
+    return render_template("reserva_mesa.html", title="Reservas - La Forchetta", year=year)
 
 @paginas_bp.route("/delivery")
 def delivery_page():
     produtos = Produto.query.all()
-    return render_template("delivery.html", title="Delivery - La Forchetta", produtos=produtos)
+    return render_template("delivery.html", title="Delivery - La Forchetta", produtos=produtos, year=year)
